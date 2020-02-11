22a156d39b2cb41dfac2300fadfcb95dcc00f160
Tatiana Eliseeva <>
2020-02-11 23:16:04 +0100

    first try

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 /**
- * Returns a very important number
- * @return {number}
+ * Mann-Whitney rank test on sets x1 and x2
+ *
+ * Parameters:
+ * x1, x2 - 1D arrays
  */
-export function myModule(a, b) {
-  return a + b;
+
+export function UTest(x1, x2) {
+  len_x1 = x1.length;
+  len_x2 = x2.length;
+  return len_x1, len_x2;
 }
+
+var array1 = [1, 3, 5];
+var array2 = [2, 4, 6];
+
+l_array1, (l_array2 = UTest(array1, array2));
