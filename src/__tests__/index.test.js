9be3b1f2c14b9e93ffc66910aac154e3096e9f7f
Tatiana Eliseeva <>
2020-02-18 11:53:25 +0100

    returns u parameters

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,7 +1,9 @@
-import { myModule } from '..';
+/*
+import { UTest } from '..';
 
 describe('test myModule', () => {
   it('should return 42', () => {
-    expect(myModule(2, 3)).toStrictEqual(5);
+    expect(UTest(2, 3)).toStrictEqual([6, 4, 2, 5, 3, 1]);
   });
 });
+*/
--- src/index.js
+++ src/index.js
@@ -6,12 +6,26 @@
  */
 
 export function UTest(x1, x2) {
-  len_x1 = x1.length;
-  len_x2 = x2.length;
-  return len_x1, len_x2;
+  var len_x1 = x1.length;
+  var len_x2 = x2.length;
+
+  var con_array = x1.concat(x2);
+  var sorted = con_array.slice().sort(function(a, b) {
+    return b - a;
+  });
+  var ranks = con_array.slice().map(function(v) {
+    return sorted.indexOf(v) + 1;
+  });
+
+  var rank_x1 = ranks.slice(0, len_x1);
+  var u1 = rank_x1.reduce((a, b) => a + b, 0);
+  var u2 = ranks.reduce((a, b) => a + b, 0) - u1;
+
+  return [u1, u2];
 }
 
 var array1 = [1, 3, 5];
 var array2 = [2, 4, 6];
 
-l_array1, (l_array2 = UTest(array1, array2));
+var r = UTest(array1, array2);
+console.log(r);
