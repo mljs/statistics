45fa1eff2fd8767b396aac3f2d3f0d71025adb40
Tatiana Eliseeva <>
2020-03-01 20:50:05 +0100

    finished tie correct

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import sum from 'ml-array-sum';
 import binarySearch from 'binary-search';
+//import { Matrix } from 'ml-matrix';
 
 /**
  * Mann-Whitney rank test on sets x1 and x2
@@ -17,15 +18,27 @@ export function TieCorrection(rankValues) {
    * OUT: factor: correction factor
    */
   const sortedArr = rankValues.slice().sort((a, b) => b - a);
-  const leftArr = sortedArr.slice(1, test_array.length);
-  const rightArr = sortedArr.slice(0, test_array.length - 1);
+  const leftArr = sortedArr.slice(1, sortedArr.length);
+  const rightArr = sortedArr.slice(0, sortedArr.length - 1);
   const nonNegative = [0, leftArr.join('').localeCompare(rightArr.join('')), 0];
   const nonNegativeIdxs = nonNegative
     .map((a, i) => (a == 0 ? i : -1))
     .filter((a) => a !== -1);
-  //const nonNegativeIdxs =
-
-  return Object;
+  var diffCounter = nonNegativeIdxs
+    .slice(1, nonNegativeIdxs.length)
+    .map(function(num, idx) {
+      return num - nonNegativeIdxs.slice(0, nonNegativeIdxs.length - 1)[idx];
+    });
+  const sortedArrSize = sortedArr.length;
+  if (sortedArrSize < 2) {
+    tieCorr = 1;
+  } else {
+    tieCorr =
+      1 -
+      (Math.pow(diffCounter, 3) - diffCounter) /
+        (Math.pow(sortedArrSize, 3) - sortedArrSize);
+  }
+  return tieCorr;
 }
 
 export function uTest(x1, x2) {
@@ -51,9 +64,13 @@ export function uTest(x1, x2) {
 }
 
 const test_array = [1, 2, 5, 6, 7];
-const test_array_2 = [0, -1, 0, 0];
+const test_array_2 = [0, -1, 0, 0, 2];
 
 console.log(test_array.slice(1, test_array.length - 1));
 console.log(
   test_array_2.map((a, i) => (a == 0 ? i : -1)).filter((a) => a !== -1),
 );
+var t = test_array.map(function(num, idx) {
+  return num - test_array_2[idx];
+});
+console.log(t[0] > 1);
