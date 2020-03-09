b6ca412c36e86a5b3e1e605e982c07ebaafb1162
Tatiana Eliseeva <>
2020-03-09 16:38:26 +0100

    fixed u parameters

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ export function TieCorrection(rankValues) {
    * OUT: factor: correction factor
    */
   const sortedArr = rankValues.slice().sort((a, b) => b - a);
+  if (sortedArr.length < 2) {
+    return 1;
+  }
   const leftArr = sortedArr.slice(1, sortedArr.length);
   const rightArr = sortedArr.slice(0, sortedArr.length - 1);
   const nonNegative = [0, leftArr.join('').localeCompare(rightArr.join('')), 0];
@@ -30,20 +33,16 @@ export function TieCorrection(rankValues) {
       return num - nonNegativeIdxs.slice(0, nonNegativeIdxs.length - 1)[idx];
     });
   const sortedArrSize = sortedArr.length;
-  var tieCorr;
-  if (sortedArrSize < 2) {
-    tieCorr = 1;
-  } else {
-    tieCorr =
-      1 -
-      (Math.pow(diffCounter, 3) - diffCounter) /
-        (Math.pow(sortedArrSize, 3) - sortedArrSize);
-  }
+  const tieCorr =
+    1 -
+    (Math.pow(diffCounter, 3) - diffCounter) /
+      (Math.pow(sortedArrSize, 3) - sortedArrSize);
   return tieCorr;
 }
 
 export function uTest(x1, x2, method) {
-  /** with default continuity correction
+  /** with default continuity correction (add later)
+   * alternative is default: none (later can be added: two-sided, greater, less)
    * Method One is used for small samples
    */
   const concatArray = x1.concat(x2);
@@ -54,26 +53,26 @@ export function uTest(x1, x2, method) {
         return needle - element;
       }) + 1,
   );
-
-  const ranksX1 = ranks.slice(0, x1.length);
-  const u1 = sum(ranksX1);
-  const u2 = sum(ranks) - u1;
+  const n1 = x1.length;
+  const n2 = x2.length;
+  const ranksX1 = ranks.slice(0, n1);
+  const u1 = n1 * n2 + (n1 * (n1 + 1)) / 2 - sum(ranksX1);
+  const u2 = n1 * n2 - u1;
   if (method == 'Simple') {
     return { u1, u2 };
   }
 
-  const T = TieCorrection(ranks);
+  const T = TieCorrection(concatArray);
   var sd = 0;
   if (T == 0) {
     console.log('The objects are identical');
   } else {
-    sd = Math.sqrt(
-      (T * x1.length * x2.length * (x1.length + x2.length + 1)) / 12.0,
-    );
+    sd = Math.sqrt((T * n1 * n2 * (n1 + n2 + 1)) / 12.0);
   }
-
-  return { u1, u2, sd, ranks };
+  const mRank = 0.5 + (n1 * n2) / 2;
+  const z = (Math.max(u1, u2) - mRank) / sd;
+  return { u1, u2, T, z };
 }
 
 console.log(TieCorrection([6, 4, 2, 5, 3, 1]));
-console.log(uTest([1, 3, 5], [2, 4, 6], 'Simple'));
+console.log(uTest([1, 3, 5], [2, 4, 6]));
