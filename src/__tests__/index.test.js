7606c13e326cc217f81e700d4c804dbc09822ecf
Tatiana Eliseeva <>
2020-03-10 12:37:10 +0100

    test check

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -4,9 +4,7 @@ describe('uTest', () => {
   it('simple case', () => {
     let array1 = [1, 3, 5];
     let array2 = [2, 4, 6];
-
     let result = uTest(array1, array2, 'Simple');
-
     expect(result).toStrictEqual({ u1: 6, u2: 3 });
   });
 });
@@ -14,9 +12,7 @@ describe('uTest', () => {
 describe('tie correction', () => {
   it('simple case with two ranks', () => {
     let ranks = [1, 3, 5, 2, 4, 6];
-
     let result = TieCorrection(ranks);
-
     expect(result).toStrictEqual(1);
   });
 });
