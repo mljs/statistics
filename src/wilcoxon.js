0e4af9feb68a445b142d3fbe4f69a55618a1b314
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-03-04 16:10:41 +0100

    refactor: cleanup

--- src/wilcoxon.js
+++ src/wilcoxon.js
@@ -2,14 +2,13 @@ import sum from 'ml-array-sum';
 
 import { ranking } from './ranking';
 
-export function wilcoxon() {
+export function wilcoxon(...args) {
   /* performes Wilcoxon test*/
-  const args = Array.from(arguments);
-  const ranks = ranking.apply(null, arguments);
-  let start_length = 0;
+  const ranks = ranking(args);
+  let startLength = 0;
   const sums = args.map((item) => {
-    start_length = start_length + item.length;
-    let uu = sum(ranks.slice(start_length - item.length, start_length));
+    startLength = startLength + item.length;
+    let uu = sum(ranks.slice(startLength - item.length, startLength));
     return uu;
   });
   return sums;
